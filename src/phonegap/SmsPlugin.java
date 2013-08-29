a506425802b76c1ab8dc7771253210430968bb5c
thangnm1610 <nguyenthang_ict@yahoo.com>
2013-08-29 15:21:37 +0700

    phonegap
    phonegap

---
+++ src/phonegap/SmsPlugin.java
@@ -0,0 +1,44 @@
+package phonegap;
+
+import org.apache.cordova.api.PluginResult.Status;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.telephony.SmsManager;
+
+import org.apache.cordova.api.Plugin;
+import org.apache.cordova.api.PluginResult;
+
+public class SmsPlugin extends Plugin {
+	public final String ACTION_SEND_SMS = "SendSMS";
+
+	@Override
+	public PluginResult execute(String action, JSONArray arg1, String callbackId) {
+		PluginResult result = new PluginResult(Status.INVALID_ACTION);
+
+		if (action.equals(ACTION_SEND_SMS)) {
+			try {
+				String phoneNumber = arg1.getString(0);
+				String message = arg1.getString(1);
+				sendSMS(phoneNumber, message);
+				result = new PluginResult(Status.OK);
+			}
+			catch (JSONException ex) {
+				result = new PluginResult(Status.JSON_EXCEPTION, ex.getMessage());
+			}			
+		}
+
+		return result;
+	}
+
+	private void sendSMS(String phoneNumber, String message) {
+		SmsManager manager = SmsManager.getDefault();
+
+        PendingIntent sentIntent = PendingIntent.getActivity(this.ctx.getContext(), 0, new Intent(), 0);  
+
+		manager.sendTextMessage(phoneNumber, null, message, sentIntent, null);
+	}
+
+}
